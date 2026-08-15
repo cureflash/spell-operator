48881b565baeccd2deb7a09dd488e7158f6eed85
HIkaru Furuta <89366952+cureflash@users.noreply.github.com>
2026-08-15 22:28:16 +0900

    Load Pipoya NPC sprite styles

--- js/game.js
+++ js/game.js
@@ -8,6 +8,7 @@
   addStyle("css/items.css?v=2");
   addStyle("css/story.css?v=1");
   addStyle("css/rpg-field-v1.css?v=3");
+  addStyle("css/npc-pipoya.css?v=1");
   const load = src => new Promise((resolve,reject)=>{ const s=document.createElement("script"); s.src=src; s.onload=resolve; s.onerror=reject; document.body.appendChild(s); });
   load("js/game03-core.js?v=6")
     .then(()=>load("js/game03-items.js?v=3"))
