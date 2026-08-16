7d3c64c464411a052dd826e01ede94a4396b68d1
HIkaru Furuta <89366952+cureflash@users.noreply.github.com>
2026-08-16 22:22:29 +0900

    Load follower normalization and refresh field core

--- js/game.js
+++ js/game.js
@@ -14,7 +14,7 @@
   addStyle("css/house-maps.css?v=1");
   addStyle("css/house-rooms-v2.css?v=1");
   addStyle("css/pokemon-house-compact.css?v=1");
-  addStyle("css/sophie-sprite.css?v=15");
+  addStyle("css/sophie-sprite.css?v=16");
   const load=src=>new Promise((resolve,reject)=>{const s=document.createElement("script");s.src=src;s.onload=resolve;s.onerror=reject;document.body.appendChild(s);});
   load("js/python-runner.js?v=1")
     .then(()=>load("js/interpreter.js?v=3"))
@@ -28,7 +28,8 @@
     .then(()=>load("js/game03-story.js?v=6"))
     .then(()=>load("js/friend-conversation.js?v=1"))
     .then(()=>load("js/game03-battle.js?v=6"))
-    .then(()=>load("js/game03-field.js?v=18"))
+    .then(()=>load("js/follower-normalize.js?v=1"))
+    .then(()=>load("js/game03-field.js?v=19"))
     .then(()=>load("js/house-room-layout.js?v=3"))
     .then(()=>load("js/party-lockstep.js?v=6"))
     .then(()=>load("js/map-scroll-fix.js?v=1"))
