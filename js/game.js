80ef912927c1aba37e5b697fbb814e54b5889f2a
HIkaru Furuta <89366952+cureflash@users.noreply.github.com>
2026-08-16 22:14:12 +0900

    Load unified party rendering

--- js/game.js
+++ js/game.js
@@ -14,7 +14,7 @@
   addStyle("css/house-maps.css?v=1");
   addStyle("css/house-rooms-v2.css?v=1");
   addStyle("css/pokemon-house-compact.css?v=1");
-  addStyle("css/sophie-sprite.css?v=14");
+  addStyle("css/sophie-sprite.css?v=15");
   const load=src=>new Promise((resolve,reject)=>{const s=document.createElement("script");s.src=src;s.onload=resolve;s.onerror=reject;document.body.appendChild(s);});
   load("js/python-runner.js?v=1")
     .then(()=>load("js/interpreter.js?v=3"))
@@ -30,7 +30,7 @@
     .then(()=>load("js/game03-battle.js?v=6"))
     .then(()=>load("js/game03-field.js?v=18"))
     .then(()=>load("js/house-room-layout.js?v=3"))
-    .then(()=>load("js/party-lockstep.js?v=5"))
+    .then(()=>load("js/party-lockstep.js?v=6"))
     .then(()=>load("js/map-scroll-fix.js?v=1"))
     .then(()=>load("js/python-polish.js?v=1"))
     .then(()=>load("js/npc-facing.js?v=1"))
