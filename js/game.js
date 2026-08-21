9a5dcfe32af7516cede73b6aee18ce2d39e2e7ee
HIkaru Furuta <89366952+cureflash@users.noreply.github.com>
2026-08-22 00:34:32 +0900

    bust dialog typewriter cache

--- js/game.js
+++ js/game.js
@@ -32,7 +32,7 @@
     .then(()=>load("js/follower-normalize.js?v=1"))
     .then(()=>load("js/game03-field.js?v=19"))
     .then(()=>load("js/character-portraits.js?v=1"))
-    .then(()=>load("js/dialog-typewriter.js?v=1"))
+    .then(()=>load("js/dialog-typewriter.js?v=2"))
     .then(()=>load("js/house-room-layout.js?v=4"))
     .then(()=>load("js/party-lockstep.js?v=6"))
     .then(()=>load("js/map-scroll-fix.js?v=1"))
