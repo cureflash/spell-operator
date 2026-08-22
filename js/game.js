13786e9f2526ded3bbe7b47898991a6c45d9ba18
HIkaru Furuta <89366952+cureflash@users.noreply.github.com>
2026-08-22 11:44:44 +0900

    Bust field menu cache for イードウ

--- js/game.js
+++ js/game.js
@@ -25,7 +25,7 @@
     .then(()=>load("js/movement-step-lock.js?v=1"))
     .then(()=>load("js/house-movement-fix.js?v=1"))
     .then(()=>load("js/game03-items.js?v=3"))
-    .then(()=>load("js/game03-menu.js?v=13"))
+    .then(()=>load("js/game03-menu.js?v=14"))
     .then(()=>load("js/game03-story.js?v=10"))
     .then(()=>load("js/friend-conversation.js?v=3"))
     .then(()=>load("js/game03-battle.js?v=6"))
