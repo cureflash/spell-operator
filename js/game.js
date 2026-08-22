c272d8fac0104f4a86b60a6f1f72ea61b5f987bd
HIkaru Furuta <89366952+cureflash@users.noreply.github.com>
2026-08-22 12:24:16 +0900

    Load Kirayuki transition before field menu

--- js/game.js
+++ js/game.js
@@ -26,6 +26,7 @@
     .then(()=>load("js/movement-step-lock.js?v=1"))
     .then(()=>load("js/house-movement-fix.js?v=1"))
     .then(()=>load("js/game03-items.js?v=3"))
+    .then(()=>load("js/plugin-transition.js?v=1"))
     .then(()=>load("js/game03-menu.js?v=17"))
     .then(()=>load("js/game03-story.js?v=10"))
     .then(()=>load("js/friend-conversation.js?v=3"))
